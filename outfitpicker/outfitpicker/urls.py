b50495329700688ebcc3ef977c90a6ef63635958
Sam Yu <smyu03@gmail.com>
2019-11-23 18:26:05 -0500

    auth

--- outfitpicker/outfitpicker/urls.py
+++ outfitpicker/outfitpicker/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('closet/', include('closet.urls')),
+    path('closet/', include('closet.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
