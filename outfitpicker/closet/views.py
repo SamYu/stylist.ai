42393f8fd268b746424e0b6bca204186b458ad00
Tony Wang <tonyy.wangg@gmail.com>
2019-11-23 17:52:01 -0500

    merge

--- outfitpicker/closet/views.py
+++ outfitpicker/closet/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from closet.models import (
+    ClothingItem,
+    Closet,
+)
 
 def index(request):
     return HttpResponse("hack western baby")
+
+
+def add_clothing_item(request):
+    user = get_auth_user()
+    closet = user.closet
+    new_clothing = ClothingItem(
+        name=request.data.get('name'),
+        type=request.data.get('type'),
+        material=request.data.get('material'),
+        colour=request.data.get('colour'),
+        closet=closet
+    )
+
+# name: 'nike shirt'
+# type: 'Top'
+# colour: 'Red'
+
