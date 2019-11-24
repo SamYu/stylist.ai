39536ad419193cd395eb886ece2c243f531f1885
Bill DuGe <billduge11@gmail.com>
2019-11-24 04:46:11 -0500

    logout function

--- outfitpicker/user/views.py
+++ outfitpicker/user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from closet.models import Closet
 from django.views.decorators.csrf import csrf_exempt
@@ -12,19 +12,24 @@ def register(request):
     # username
     # email
     # password
-    json_data = json.loads(request.body)
-    username = json_data["username"]
-    email = json_data["email"]
-    password = json_data["password"]
-    new_user = User.objects.create_user(
-        username=username, email=email, password=password)
-    new_user.save()
+    try:
+        json_data = json.loads(request.body)
+        username = json_data["username"]
+        email = json_data["email"]
+        password = json_data["password"]
+        new_user = User.objects.create_user(
+            username=username, email=email, password=password)
+        new_user.save()
 
-    new_closet = Closet(user=new_user, count=0)
-    new_closet.save()
-    response = HttpResponse()
-    response.status_code = 200
-    return response
+        new_closet = Closet(user=new_user, count=0)
+        new_closet.save()
+        response = HttpResponse()
+        response.status_code = 200
+        return response
+    except:
+        response = HttpResponse()
+        response.status_code = 320
+        return response
 
 
 @csrf_exempt
@@ -45,3 +50,11 @@ def loginUser(request):
     else:
         response.status_code = 401
         return response
+
+
+@csrf_exempt
+def logoutUser(request):
+    logout(request)
+    response = HttpResponse()
+    response.status_code = 200
+    return response
