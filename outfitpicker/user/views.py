2878c3d509453beaa8145c57a25c94819606b142
Bill DuGe <billduge11@gmail.com>
2019-11-24 05:16:47 -0500

    debug

--- outfitpicker/user/views.py
+++ outfitpicker/user/views.py
@@ -12,24 +12,19 @@ def register(request):
     # username
     # email
     # password
-    try:
-        json_data = json.loads(request.body)
-        username = json_data["username"]
-        email = json_data["email"]
-        password = json_data["password"]
-        new_user = User.objects.create_user(
-            username=username, email=email, password=password)
-        new_user.save()
+    json_data = json.loads(request.body)
+    username = json_data["username"]
+    email = json_data["email"]
+    password = json_data["password"]
+    new_user = User.objects.create_user(
+        username=username, email=email, password=password)
+    new_user.save()
 
-        new_closet = Closet(user=new_user, count=0)
-        new_closet.save()
-        response = HttpResponse()
-        response.status_code = 200
-        return response
-    except:
-        response = HttpResponse()
-        response.status_code = 320
-        return response
+    new_closet = Closet(user=new_user, count=0)
+    new_closet.save()
+    response = HttpResponse()
+    response.status_code = 200
+    return response
 
 
 @csrf_exempt
