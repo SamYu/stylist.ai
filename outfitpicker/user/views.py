61fda1cb54396fdce68479e916dccc08cc6ca678
Bill DuGe <billduge11@gmail.com>
2019-11-23 21:33:58 -0500

    Added login authentication

--- outfitpicker/user/views.py
+++ outfitpicker/user/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.http import HttpResponse
 from closet.models import Closet
+import json
 
 
 def register(request):
     # username
     # email
     # password
-    username = request.body.username
-    email = request.body.email
-    password = request.body.password
+    json_data = json.loads(request.body)
+    username = json_data["username"]
+    email = json_data["email"]
+    password = json_data["password"]
     new_user = User.objects.create_user(
         username=username, email=email, password=password)
+    new_user.save()
 
     new_closet = Closet(user=new_user, count=0)
     new_closet.save()
@@ -22,4 +26,15 @@ def register(request):
 def login(request):
     # username
     # password
-    return
+    json_data = json.loads(request.body)
+    username = json_data["username"]
+    password = json_data["password"]
+
+    user = authenticate(username=username, password=password)
+    response = HttpResponse()
+    if user is not None:
+        response.status_code = 200
+        return response
+    else:
+        response.status_code = 401
+        return response
